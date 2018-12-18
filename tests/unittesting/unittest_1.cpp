ef9fc6e86c96387bf8a8517ad9c56fcb2780ba91
nikita <nikita.mahto@alten.nl>
2018-12-18 11:31:54 +0100

    Updating unit test files

--- tests/unittesting/unittest_1.cpp
+++ tests/unittesting/unittest_1.cpp
@@ -1,7 +1,17 @@
 #include <gtest/gtest.h>
+#include <utilityFunctions.h>
 
 
-TEST {
+TEST (HelperFunctionTest, TwoValues){
+    ASSERT_EQ(19.2094, dist(12,15));
+    ASSERT_EQ(6., dist(4.,4.));
 
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    int ret = RUN_ALL_TESTS();
+    return ret;
 }
 
