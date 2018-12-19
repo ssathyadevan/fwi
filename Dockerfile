9af414ad72a49e6b7bd67e41b1dba926cc7afb81
nikita <nikita.mahto@alten.nl>
2018-12-19 14:11:40 +0100

    Fixing jenkisn files for BM

--- Dockerfile
+++ Dockerfile
@@ -1,69 +1,36 @@
 FROM ubuntu:xenial
 
 # Create jenkins
-
 RUN groupadd -g 10000 jenkins && \
-
         useradd -c "Jenkins user" -d /home/jenkins -u 10000 -g 10000 -m jenkins
-
 # Give jenkins user root privileges
-
 RUN usermod -aG sudo jenkins
-
 RUN su jenkins
-
 WORKDIR /home/jenkins
-
 # update and install dependencies
-
 RUN         apt-get update \
-
                 && apt-get install -y \
-
                     software-properties-common \
-
                     wget \
-
                     g++\
-
                     make \
-
                     vim \
-
                     git \
-
                     cmake=3.5.1-1ubuntu3
 
 RUN add-apt-repository ppa:ubuntu-toolchain-r/test -y
-
 RUN apt-get update && apt-get -y install gcc-6 g++-6
-
 RUN update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6
-
 RUN gcc -v
-
 RUN mkdir googleTest ; cd googleTest ; mkdir googletest-src googletest-build
-
 RUN cd googleTest/googletest-src; git clone https://github.com/google/googletest.git
-
 RUN cd googleTest/googletest-build ; cmake -DCMAKE_INSTALL_PREFIX=${PWD}/../install ../googletest-src/googletest
-
 RUN cd googleTest/googletest-build ; make all install -j8
 
 # Declare here the ENV variables that you wish to be able to overwrite in build time
-
 ARG GTEST_ROOT=/home/jenkins/googleTest/install
-
-ARG BM_SRC_DIR=/home/jenkins
-
-ARG BM_BUILD_DIR=/home/jenkins/build
 
 # Here the sets up the variables to be overwritten if specified value on build time.
 
 ENV GTEST_ROOT=$GTEST_ROOT
 
-ENV BM_SRC_DIR=$BM_SRC_DIR
-
-ENV BM_BUILD_DIR=$BM_BUILD_DIR
-
-ENV PATH=$PATH:${BM_SRC_DIR}/bin
--- jenkinsFunctions/functions.groovy
+++ jenkinsFunctions/functions.groovy
@@ -7,39 +7,23 @@ import hudson.model.Actionable
 def setEnvironment() {
 
         // Get commit parameters like commit code and author
-
         env.SHORT_COMMIT_CODE = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
-
         env.COMMIT_MESSAGE = sh(returnStdout: true, script: "git log --format=%B -n 1 ${SHORT_COMMIT_CODE}").trim()
-
         env.COMITTER_EMAIL = sh(returnStdout: true, script: 'git --no-pager show -s --format=\'%ae\'').trim()
-
         env.AUTHOR_NAME = sh(returnStdout: true, script: 'git --no-pager show -s --format=\'%an\'').trim()
-
         // Set build name and description accordingly
-
         currentBuild.displayName = "BM | commit ${SHORT_COMMIT_CODE} | ${AUTHOR_NAME}"
-
         currentBuild.description = "${COMMIT_MESSAGE}"
-
 }
 
 def buildAll() {
-
         echo 'Building..'
-
         sh '''
-
-                mkdir build
-
-                cd build
-
-                cmake -DCMAKE_BUILD_TYPE=Release ../parallelized-fwi
-
-                make
-
+        mkdir build
+        cd build
+        cmake -DCMAKE_BUILD_TYPE=Release ..
+        make
         '''
-
 }
 
 
@@ -51,37 +35,26 @@ def deploy(){
                 --exclude="Jenkinsfile" \
                 --zcf BM-${GIT_BRANCH}-${SHORT_COMMIT_CODE}.tar.gz *'
 
-        archiveArtifacts artifacts:"BM-${GIT_BRANCH}-${SHORT_COMMIT_CODE}.tar.gz"
+        archiveArtifacts artifacts:"FWI-${GIT_BRANCH}-${SHORT_COMMIT_CODE}.tar.gz"
 
 }
 
 def sendEmail() {
 
         email = evaluate readTrusted('jenkinsFunctions/email.groovy')
-
-
-
         if(currentBuild.currentResult == "UNSTABLE" || currentBuild.currentResult == "SUCCESS") {
 
                 testSummaryLib = evaluate readTrusted('jenkinsFunctions/testSummary.groovy')
-
                 testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
-
                 testSummary =  testSummaryLib.getTestSummary(testResultAction)
-
                 testResultAction = null
-
                 email.sendEmail(testSummary)
-
         }
 
         if(currentBuild.currentResult == "FAILURE") {
 
                 email.sendEmailFailure()
-
         }
-
 }
-
 return this
 
