e59437de31aa2af9c2bd36f7da7711e7a42b02b2
saurabh <saurabh.sharma@alten.nl>
2018-12-24 15:34:17 +0100

    trial test

--- jenkinsFunctions/testSummary.groovy
+++ jenkinsFunctions/testSummary.groovy
@@ -15,7 +15,7 @@ def getTestSummary(testResultAction) {
                 failedTestString = ""
 
                 summary = "Test results:\n\t"
-                summary = summary + ("Total: " + total)
+                /*summary = summary + ("Total: " + total)
                 summary = summary + (", Passed: " + (total - failed - skipped))
                 summary = summary + (", Failed: " + failed + " " + failDiffString)
                 summary = summary + (", Skipped: " + skipped)
@@ -26,7 +26,7 @@ def getTestSummary(testResultAction) {
 
                         summary = summary + "\n\nFailed tests:\n"
                         summary = summary + failedTestString
-                }
+                }*/
         }
         else {
                 summary = "No tests found"
