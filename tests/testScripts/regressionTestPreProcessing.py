57120a1675f58b62705537764d3e0a7bfa0f1443
U-PROD\nmahto <nmahto@ALTEN-C085.prod.altengroup.dir>
2019-01-11 14:27:31 +0100

    fixing python scripts for jenkins

--- tests/testScripts/regressionTestPreProcessing.py
+++ tests/testScripts/regressionTestPreProcessing.py
@@ -1,8 +1,9 @@
 
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-import sys, os, difflib, filecmp, csv, numpy, matplotlib, shutil, matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+import sys, os, difflib, filecmp, csv, numpy, shutil, matplotlib.pyplot as plt
 from datetime import datetime
 
 
