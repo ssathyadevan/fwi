53637482552da7fc9d2fefb78a481882576534d1
Leonard Huijzer <leonard.huijzer@alten.nl>
2019-02-26 13:39:26 +0100

    refs #8993 Updated input files of regression test to match current format. Script running all regression tests now works

--- tests/testScripts/run_all_regression_tests.sh
+++ tests/testScripts/run_all_regression_tests.sh
@@ -12,42 +12,25 @@ cp ../parallelized-fwi/tests/testScripts/* test/ 2>/dev/null
 NEW="NEW"
 for TEST in $TESTS
 do
-	rm output/* test/* 2>/dev/null
 	echo "Running test:" $TEST
 	cp ../parallelized-fwi/tests/regression_data/$TEST/$TEST.in input/"$TEST$NEW.in"
 	cp ../parallelized-fwi/tests/regression_data/$TEST/* test/
 	cp input/"$TEST$NEW.in" test/
 
-	./bin/FWI_PreProcess ../input/ ../output/ $TEST$NEW
-	./bin/FWI_Process ../input/ ../output/ $TEST$NEW
-	break
+	./bin/FWI_PreProcess ./input/ ./output/ $TEST$NEW
+	./bin/FWI_Process ./input/ ./output/ $TEST$NEW
+
+	cp output/* test/
 
 	python3 postProcessing-python3.py output/
-	python3 test/regressionTestPreProcessing_python3.py "$TEST$NEW" $TEST
-	python3 test/regressionTestProcessing_python3.py "$TEST$NEW" $TEST	
+	cd test/
+	python3 regressionTestPreProcessing_python3.py $TEST $TEST$NEW
+	python3 regressionTestProcessing_python3.py $TEST $TEST$NEW
+	cd ..
+
+	rm output/* test/* 2>/dev/null
+	exit
 done	
 
-exit
-# Run unit tests
-python3 -m pytest test/python_unittest.py --junitxml results.xml
-cp results.xml ../build
-rm -r input/ output/ test/ 
-
-#cp inputFiles/* input/
-#cd FWIInstall/bin
-#./FWI_PreProcess ../../input/ ../../output/ default
-#./FWI_Process ../../input/ ../../output/ default
-#cd ../../pythonScripts
-#cp postProcessing-python3.py ../
-#cd ..
-#python3 postProcessing-python3.py output/
-#mkdir test
-#cp tests/regression_data/fast/* test/
-#cp tests/testScripts/* test/
-#cp input/default.in test/
-#cp output/* test/
-#cd test
-#python3 regressionTestPreProcessing_python3.py fast default
-#python3 regressionTestProcessing_python3.py fast default
-#python3 -m pytest python_unittest.py --junitxml results.xml 
-#cp results.xml ../build/
+# Clean up
+rm -r input/ output/ test/
