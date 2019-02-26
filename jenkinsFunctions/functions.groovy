d7567ca50ab7b7e838048ab36ed80babe4f71810
Leonard Huijzer <leonard.huijzer@alten.nl>
2019-02-26 17:00:43 +0100

    refs 8993 Update Jenkins

--- jenkinsFunctions/functions.groovy
+++ jenkinsFunctions/functions.groovy
@@ -41,7 +41,7 @@ def testAll() {
 
 def regressiontest() {
         echo 'Running regression tests'
-	sh -v tests/testScripts/run_all_regression_tests.sh	
+	bash tests/testScripts/run_all_regression_tests.sh	
 }
 
 def deploy(){
