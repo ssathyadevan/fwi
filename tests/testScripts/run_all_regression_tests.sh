6eebe88b542790d3161d517101bfb6529e48b3b6
Leonard Huijzer <leonard.huijzer@alten.nl>
2019-02-26 14:37:52 +0100

    refs #8993 Modified Jenkins files to now run all regression tests using run_all_regression_tests.sh

--- tests/testScripts/run_all_regression_tests.sh
+++ tests/testScripts/run_all_regression_tests.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
 cd ../../..
 
+#FWI_INSTALL_PATH=~/FWIInstall
+#FWI_SOURCE_PATH=~/parallelized-fwi
+FWI_INSTALL_PATH=/var/jenkins_home/workspace/FWI/${GIT_BRANCH}/FWIInstall
+FWI_SOURCE_PATH=/var/jenkins_home/workspace/FWI/${GIT_BRANCH}/
+
 # Run all regression tests
-cd ./parallelized-fwi/tests/regression_data
+cd $FWI_SOURCE_PATH/tests/regression_data
 TESTS=$(find . -maxdepth 1 -type d ! -path . -printf '%P\n')
-cd ../../../FWIInstall
+cd $FWI_INSTALL_PATH/
 mkdir -p input output test
-cp ../parallelized-fwi/inputFiles/* input/
-cp ../parallelized-fwi/pythonScripts/postProcessing-python3.py .
-cp ../parallelized-fwi/tests/testScripts/* test/ 2>/dev/null
+cp $FWI_SOURCE_PATH/inputFiles/* input/
+cp $FWI_SOURCE_PATH/pythonScripts/postProcessing-python3.py .
 NEW="NEW"
 for TEST in $TESTS
 do
 	echo "Running test:" $TEST
-	cp ../parallelized-fwi/tests/regression_data/$TEST/$TEST.in input/"$TEST$NEW.in"
-	cp ../parallelized-fwi/tests/regression_data/$TEST/* test/
+
+	cp $FWI_SOURCE_PATH/tests/testScripts/* test/ 2>/dev/null
+	cp $FWI_SOURCE_PATH/tests/regression_data/$TEST/$TEST.in input/"$TEST$NEW.in"
+	cp $FWI_SOURCE_PATH/tests/regression_data/$TEST/* test/
 	cp input/"$TEST$NEW.in" test/
 
-	./bin/FWI_PreProcess ./input/ ./output/ $TEST$NEW
-	./bin/FWI_Process ./input/ ./output/ $TEST$NEW
+	./bin/FWI_PreProcess input/ output/ $TEST$NEW
+	./bin/FWI_Process input/ output/ $TEST$NEW
 
 	cp output/* test/
 
 	python3 postProcessing-python3.py output/
+
 	cd test/
 	python3 regressionTestPreProcessing_python3.py $TEST $TEST$NEW
 	python3 regressionTestProcessing_python3.py $TEST $TEST$NEW
+	python3 -m pytest python_unittest.py --junitxml results.xml
 	cd ..
 
 	rm output/* test/* 2>/dev/null
-	exit
 done	
 
 # Clean up
