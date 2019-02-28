74260085057c9fe89f21ea52ed7f28cf0c9b622c
Renger <renger.zoonen@alten.nl>
2019-02-28 18:25:00 +0100

    testing tests

--- tests/unittesting/cginversiontest.cpp
+++ tests/unittesting/cginversiontest.cpp
@@ -5,15 +5,15 @@
 //Generic Input Card test:
 TEST(CGInversionInputTest, testCardToInputFunctionality){
     ASSERT_TRUE(true);
-//    std::string temp = "./";
-//    conjugateGradientInversionInputCardReader cardReader = conjugateGradientInversionInputCardReader(temp);
-//    conjugateGradientInput input = cardReader.getInput();
+    std::string temp = "../../../inputFiles/test/";
+    conjugateGradientInversionInputCardReader cardReader = conjugateGradientInversionInputCardReader(temp);
+    conjugateGradientInput input = cardReader.getInput();
 
-//    EXPECT_EQ(input.iteration1.tolerance, 1e-08);
-//    EXPECT_EQ(input.dAmplification.start, 5e-5);
-//    EXPECT_EQ(input.dAmplification.slope, 10.0);
-//    EXPECT_EQ(input.n_max, 5);
-//    EXPECT_EQ(input.iteration1.n, 10);
-//    EXPECT_EQ(input.doReg, true);
+    EXPECT_EQ(input.iteration1.tolerance, 1e-08);
+    EXPECT_EQ(input.dAmplification.start, 5e-5);
+    EXPECT_EQ(input.dAmplification.slope, 10.0);
+    EXPECT_EQ(input.n_max, 5);
+    EXPECT_EQ(input.iteration1.n, 10);
+    EXPECT_EQ(input.doReg, true);
 }
 
--- tests/unittesting/genericinputtest.cpp
+++ tests/unittesting/genericinputtest.cpp
@@ -4,19 +4,15 @@
 
 //Generic Input Card test:
 TEST(GenericInputTest, ints){
-    ASSERT_TRUE(true);
+    std::string temp = "../../../inputFiles/test/";
 
-//    std::string temp = "../FWIInstall/input/test/";
+    genericInputCardReader cardReader = genericInputCardReader(temp, "../FWIInstall/output/", "test");
+    genericInput input = cardReader.getInput();
 
-//    genericInputCardReader cardReader = genericInputCardReader(temp, "../FWIInstall/output/", "test");
-//    genericInput input = cardReader.getInput();
-
-//
-
-//    EXPECT_EQ(input.ngrid[0],64);
-//    EXPECT_EQ(input.ngrid[1],32);
-//    EXPECT_EQ(input.freq.nTotal, 15);
-//    EXPECT_EQ(input.nSourcesReceivers.rec, 17);
-//    EXPECT_EQ(input.nSourcesReceivers.src, 17);
+    EXPECT_EQ(input.ngrid[0],64);
+    EXPECT_EQ(input.ngrid[1],32);
+    EXPECT_EQ(input.freq.nTotal, 15);
+    EXPECT_EQ(input.nSourcesReceivers.rec, 17);
+    EXPECT_EQ(input.nSourcesReceivers.src, 17);
 
 }
