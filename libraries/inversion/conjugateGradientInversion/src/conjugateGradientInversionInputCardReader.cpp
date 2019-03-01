b229761a4d2b9812dc43111f48c1a255ac9b6956
Renger <renger.zoonen@alten.nl>
2019-03-01 16:29:46 +0100

    jenkinstest

--- libraries/inversion/conjugateGradientInversion/src/conjugateGradientInversionInputCardReader.cpp
+++ libraries/inversion/conjugateGradientInversion/src/conjugateGradientInversionInputCardReader.cpp
@@ -17,36 +17,14 @@ void conjugateGradientInversionInputCardReader::readCard(std::string inputCardPa
 	nlohmann::json j;
 	in >> j;
 	
-	conjugateGradientInput jsonInput
+    conjugateGradientInput input
 	{
 		{j["Iter1"]["n"], j["Iter1"]["tolerance"]},
         {j["DeltaAmplification"]["start"], j["DeltaAmplification"]["slope"]},
         j["do_reg"],
         j["n_max"]
 	};
-	
-    std::vector<std::string> input_parameters = readFile(inputCardPath + "ConjugateGradientInversionInput.in");
-    int parameterCounter = 0;
 
-    const double tol1_to_be_implemented               = stod(input_parameters[parameterCounter]);    ++parameterCounter; //
-    const double delta_amplification_start            = stod(input_parameters[parameterCounter]);    ++parameterCounter; //
-    const double delta_amplification_slope            = stod(input_parameters[parameterCounter]);    ++parameterCounter; //
-    const int    n_max                                = stoi(input_parameters[parameterCounter]);    ++parameterCounter;
-    const int    n_iter1                              = stoi(input_parameters[parameterCounter]);    ++parameterCounter;
-    const bool   do_reg                               = InputStringToBool(input_parameters[parameterCounter]); ++ parameterCounter;
-
-    conjugateGradientInput input
-    {
-        {n_iter1,                   tol1_to_be_implemented},
-        {delta_amplification_start, delta_amplification_slope},
-        do_reg,
-        n_max
-    };
-
-    if(jsonInput.doReg != input.doReg)
-        std::cout << "input not the same" << std::endl;
-
-    //_input = input;
-    _input = jsonInput;
+    _input = input;
 }
 
