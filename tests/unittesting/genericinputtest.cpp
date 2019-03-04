1600953da92d68c742cd47eedb7945b7eeac14c6
Renger <renger.zoonen@alten.nl>
2019-03-04 14:07:00 +0100

    fix jenkins test

--- tests/unittesting/genericinputtest.cpp
+++ tests/unittesting/genericinputtest.cpp
@@ -4,14 +4,14 @@
 
 //Generic Input Card test:
 TEST(GenericInputTest, ints){
-    std::string temp = "../../../inputFiles/test/";//jenkins path
-    //std::string temp = "../../../parallelized-fwi/inputFiles/test/";//qt path
+    std::string temp = "../../../inputFiles/test/"; //jenkins path
+    //std::string temp = "../../../parallelized-fwi/inputFiles/test/"; //qt path
 
     genericInputCardReader cardReader = genericInputCardReader(temp, "../FWIInstall/output/", "test");
     genericInput input = cardReader.getInput();
 
-    EXPECT_EQ(input.ngrid[0],64);
-    EXPECT_EQ(input.ngrid[1],32);
+    EXPECT_EQ(input.ngrid[0], 64);
+    EXPECT_EQ(input.ngrid[1], 32);
     EXPECT_EQ(input.freq.nTotal, 15);
     EXPECT_EQ(input.nSourcesReceivers.rec, 17);
     EXPECT_EQ(input.nSourcesReceivers.src, 17);
