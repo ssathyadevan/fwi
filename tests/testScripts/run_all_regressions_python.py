e3facf3d38ba981efd0c14254985d7e3e7d174da
Anna Ramolini <anna.ramolini@alten.nl>
2019-03-29 16:14:28 +0100

    trying again

--- tests/testScripts/run_all_regressions_python.py
+++ tests/testScripts/run_all_regressions_python.py
@@ -55,8 +55,8 @@ for test in tests:
     shutil.copytree(FWI_SOURCE_PATH+ "/tests/regression_data/{}/input".format(test), cwd+"/{}RUN/input".format(test))
     shutil.copytree(FWI_SOURCE_PATH+ "/tests/regression_data/{}/output".format(test), cwd+"/{}RUN/output".format(test))
 
-    os.system(FWI_INSTALL_PATH+"bin/FWI_PreProcess.exe {}RUN".format(test))
-    os.system(FWI_INSTALL_PATH+"bin/FWI_Process.exe {}RUN".format(test))
+    os.system(FWI_INSTALL_PATH+"bin/FWI_PreProcess {}RUN".format(test))
+    os.system(FWI_INSTALL_PATH+"bin/FWI_Process {}RUN".format(test))
     os.system("python3 regressionTestPreProcessing_python3.py {} {}RUN".format(test,test))
 
     destdir = "Regression_results.txt"
