56ffd35f08f8985a9c42bca00f1e0d0f361a3c10
Noud van Herpen <noud.van.herpen@alten.nl>
2019-10-30 11:18:53 +0100

    Copying inputFiles at the end is now sudo. Permission was denied before.

--- Build.py
+++ Build.py
@@ -33,13 +33,13 @@ if sys.platform.startswith('linux'):
         os.mkdir('Build')
     os.chdir(home + '/Build')
     check = os.system('sudo cmake -DCMAKE_BUILD_TYPE=Release ' +
-                        '-DCMAKE_INSTALL_PREFIX= '+ home +'/FWIInstall '+ home +'/parallelized-fwi/')
+                        '-DCMAKE_INSTALL_PREFIX='+ home +'/FWIInstall '+ home +'/parallelized-fwi/')
     checking_for_errors(check)
     check = os.system('sudo make install')
     checking_for_errors(check)
 
     if not os.path.isdir(home + '/FWIInstall'):
         os.mkdir(home + '/FWIInstall')
-    os.system('cp -r '+ home + '/parallelized-fwi/inputFiles/default/ '+ home +'/FWIInstall')
-    os.system('cp -r '+ home +'/Build/runtime/bin/ '+ home +'/FWIInstall')
+    os.system('sudo cp -r '+ home + '/parallelized-fwi/inputFiles/default/ '+ home +'/FWIInstall')
+    os.system('sudo cp -r '+ home +'/Build/runtime/bin/ '+ home +'/FWIInstall')
     
