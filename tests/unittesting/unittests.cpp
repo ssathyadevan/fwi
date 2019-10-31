be4e08539802f5385452dac43abb0a8af5c2aff3
Noud van Herpen <noud.van.herpen@alten.nl>
2019-10-31 14:56:19 +0100

    Unit Test: started working on Integral Forward Model Test, but not working yet due to a missing assertion.

--- tests/unittesting/unittests.cpp
+++ tests/unittesting/unittests.cpp
@@ -7,6 +7,12 @@
 #include <freq.h>
 #include <frequenciesGroup.h>
 
+#include <iter2.h>
+#include <integralForwardModel.h>
+#include <integralForwardModelInput.h>
+#include <forwardModelInterface.h>
+#include <greensSerial.h>
+
 /* TEMPLATE
 
 TEST(CoreTest, )
@@ -45,7 +51,7 @@ TEST(CoreTest, ReceiverTest)
 TEST(CoreTest, SourceTest)
 {
     std::array<double, 2> xMin{0, 0};
-    std::array<double, 2> xMax{10, 0};
+    std::array<double, 2> xMax{10, 0};.
     int nSrc = 6;
     sources sources(xMin, xMax, nSrc);
 
@@ -64,3 +70,39 @@ TEST(CoreTest, FrequenciesGroupTest)
 
     EXPECT_NEAR(freq_group.k[9], 0.063, 0.01); // 2*pi*20/2000 ~ 0.063
 }
+
+/*TEST(CoreTest, IntegralForwardModelTest)
+{
+    // test configuration:
+    //  ssssssrrrrr
+    //  -----------
+    //  |  10x10  |
+    //  -----------   
+    //
+    double x_min_grid[2] = {0, 0};
+    double x_max_grid[2] = {10, 10};
+    int n_x_grid[2] = {10, 10};
+    grid2D grid(x_min_grid, x_max_grid, n_x_grid);
+    
+    double x_min_src[2] = {0, 0};
+    double x_max_src[2] = {5, 0};
+    int n_src[2] = {6, 1};
+    sources sources(x_min_src, x_max_src, n_src);
+
+    double x_min_recv[2] = {6, 0};
+    double x_max_recv[2] = {10, 0};
+    int n_recv[2] = {5, 0};
+    receivers receivers(x_min_recv, x_max_recv, n_recv);
+
+    double f_min{10}, f_max{20};
+    int n_freqs = 10;
+    Freq freq{min, max, n};
+    frequenciesGroup freq_group{freq, 2000};
+
+    Iter2 iter{15, 5.0e-5, false};
+    integralForwardModelInput ifm_input{iter};
+
+    IntegralForwardModel if_model{grid, sources, receivers, freq_group, ifm_input};
+    if_model.calculateKappa();
+    if_model.calculateResidual(pressureFieldSerial chi_est(grid), )
+}*/
