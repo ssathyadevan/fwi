04703f6d88a32eb93416a866f62871abf27fc98a
Inaki Martin Soroa <inaki.martin.soroa@alten.nl>
2019-11-13 16:32:06 +0100

    MPI implemented, all threads do the full GetUpdatededDirection()

--- libraries/core/include/utilityFunctions.h
+++ libraries/core/include/utilityFunctions.h
@@ -3,13 +3,26 @@
 #include <iostream>
 #include <cmath>
 #include <string>
+#include <sstream>
 #include <vector>
+#include <mpi.h>
 
 inline void WriteToFileNotToTerminal(std::string outputLocation, std::string runName, std::string postfix)
 {
-    std::cout << "Printing the program output onto a file named: " + runName + postfix + ".out in the output folder" << std::endl;
-    std::string tempString = outputLocation + runName + postfix + ".out";
-    if (freopen(tempString.c_str(),"w", stdout)) {}
+  if (postfix == "Process"){
+    int mpi_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+      std::cout << "Printing the program output onto a file named: " + runName << mpi_rank << postfix + ".out in the output folder" << std::endl;
+      std::stringstream ss;
+      ss << outputLocation + runName << mpi_rank << postfix + ".out";
+      std::string tempString = ss.str();
+      if (freopen(tempString.c_str(),"w", stdout)) {}
+      if (mpi_rank>0) return;
+  }
+      std::cout << "Printing the program output onto a file named: " + runName + postfix + ".out in the output folder" << std::endl;
+      std::string tempString = outputLocation + runName + postfix + ".out";
+      if (freopen(tempString.c_str(),"w", stdout)) {}
+
 }
 
 inline double dist(double x, double z)
@@ -20,8 +33,8 @@ inline double dist(double x, double z)
 inline std::vector<std::string> returnInputDirectory(int c, char** v = NULL)
 {
         if(c < 2) {
-                std::vector<std::string> arguments = {"./"};
-                return arguments;
+        	std::vector<std::string> arguments = {"./"};
+            return arguments;
         }
         std::vector<std::string> arguments(v+1, c+v);
         return arguments;
