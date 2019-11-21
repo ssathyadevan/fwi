91cb7fdec436a42faa29650fe5d4e8599186bf69
Chris Zevenbergen <chris.zevenbergen@alten.nl>
2019-11-21 13:36:20 +0100

    implemented unit testing for receivers class

--- libraries/core/test/sourcesTest.cpp
+++ libraries/core/test/sourcesTest.cpp
@@ -2,16 +2,6 @@
 #include <iterator>
 #include "sources.h"
 
-// Grid2D getGrid()
-// {
-//     std::array<double, 2> x_min = {0.0, 0.0};
-//     std::array<double, 2> x_max = {2.0, 2.0};
-//     std::array<int, 2> n_x = {2,4};
-
-//     Grid2D grid(x_min, x_max, n_x);
-//     return grid;
-// }
-
 TEST(SourcesTest, ConstructorTest)
 {
     std::array<double, 2> xMin = {0.0, 0.0};
@@ -23,8 +13,8 @@ TEST(SourcesTest, ConstructorTest)
 
     for (std::vector<std::array<double, 2>>::iterator iter = sources.xSrc.begin(); iter != sources.xSrc.end(); ++iter)
     {
-        EXPECT_NEAR(iter->front(), iteration_counter*dx, 0.001);
-        EXPECT_NEAR(iter->back(), iteration_counter*dx, 0.001);
+        EXPECT_NEAR(iter->front(), iteration_counter * dx, 0.001);
+        EXPECT_NEAR(iter->back(), iteration_counter * dx, 0.001);
         ++iteration_counter;
     }
 }
