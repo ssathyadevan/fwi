f94842473f4de59d9c11195e27cd5a060fb2a124
Inaki Martin Soroa <inaki.martin.soroa@alten.nl>
2019-11-22 14:30:57 +0100

    removing useless files

--- applications/preProcessing/CMakeFiles/FWI_PreProcess.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for FWI_PreProcess.
-# This may be replaced when dependencies are built.
--- libraries/inversion/conjugateGradientInversion/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chris/Documents/parallelized-fwi/libraries/inversion/conjugateGradientInversion/test
-# Build directory: /home/chris/Documents/parallelized-fwi/libraries/inversion/conjugateGradientInversion/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
