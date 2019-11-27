baf2e81f9a7cc8c4fd544f532709340b8211f6ec
Chris Zevenbergen <chris.zevenbergen@alten.nl>
2019-11-27 10:12:24 +0100

    fixed Build.py and Run.py

--- Build.py
+++ Build.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import os
 import sys
 import json
@@ -5,6 +6,7 @@ from pythonScripts.buildandrunfunctions import *
 
 if sys.platform.startswith('linux'):
     current_directory = os.getcwd()
+
     install_gtest()
 
     print('Build project:')
@@ -12,8 +14,9 @@ if sys.platform.startswith('linux'):
     if not os.path.isdir(current_directory[:current_directory.rfind('/')] + '/Build'):
         os.mkdir('Build')
     os.chdir(current_directory[:current_directory.rfind('/')] + '/Build')
-    check = os.system('cmake -DCMAKE_BUILD_TYPE=Release ' +
-                      '-DCMAKE_INSTALL_PREFIX=../FWIInstall ../parallelized-fwi/')
+    check = os.system('sudo cmake -D CMAKE_BUILD_TYPE=Release ' +
+                    '-DCMAKE_INSTALL_PREFIX= ../FWIInstall ../parallelized-fwi/')
     checking_for_errors(check, current_directory)
-    check = os.system('make install')
+    check = os.system('sudo make install')
     checking_for_errors(check, current_directory)
+        
