5a4044ba2ccc9ddf6f59228a5d18565adc8f64af
Richard <richard.faasse@alten.nl>
2020-02-17 11:20:34 +0100

    Added tests for the grid2D class and small refactoring of PressureFieldComplexSerial

--- libraries/core/test/grid2DTest.cpp
+++ libraries/core/test/grid2DTest.cpp
@@ -50,3 +50,54 @@ TEST(grid2DTest, getDomainAreaTest)
 {
     EXPECT_NEAR(grid.GetDomainArea(), (x_max[0] - x_min[0]) * (x_max[1] - x_min[1]), 0.01);
 }
+
+TEST(grid2DTest, booleanOperatorEqualTest)
+{
+    Grid2D grid1 = Grid2D(x_min, x_max, n_x);
+    Grid2D grid2 = Grid2D(x_min, x_max, n_x);
+
+    bool gridsAreEqual = grid1 == grid2;
+
+    EXPECT_TRUE(gridsAreEqual);
+}
+
+TEST(grid2DTest, booleanOperatorUnequalXMinTest)
+{
+    Grid2D grid1 = Grid2D(x_min, x_max, n_x);
+
+    std::array<double, 2> x_min2 = {0.1, 0.3};
+    Grid2D grid2 = Grid2D(x_min2, x_max, n_x);
+
+    bool gridsAreEqual = grid1 == grid2;
+
+    EXPECT_FALSE(gridsAreEqual);
+}
+
+TEST(grid2DTest, booleanOperatorUnequalXMaxTest)
+{
+    Grid2D grid1 = Grid2D(x_min, x_max, n_x);
+
+    std::array<double, 2> x_max2 = {2.2, 1.8};
+    Grid2D grid2 = Grid2D(x_min, x_max2, n_x);
+
+    bool gridsAreEqual = grid1 == grid2;
+
+    EXPECT_FALSE(gridsAreEqual);
+}
+
+TEST(grid2DTest, booleanOperatorUnequalDxTest)
+{
+    Grid2D grid1 = Grid2D(x_min, x_max, n_x);
+
+    std::array<int, 2> n_x2 = {3, 6};
+    Grid2D grid2 = Grid2D(x_min, x_max, n_x2);
+
+    bool gridsAreEqual = grid1 == grid2;
+
+    EXPECT_FALSE(gridsAreEqual);
+}
+
+
+
+
+
