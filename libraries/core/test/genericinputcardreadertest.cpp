f7f4a8a44e05f3dc407b736fc14860f7d81a1567
Richard Faasse <richard.faasse@alten.nl>
2020-03-02 13:59:08 +0100

    Added unittests for the inputreader, which work in QTcreator, but won't work on jenkins just yet

---
+++ libraries/core/test/genericinputcardreadertest.cpp
@@ -0,0 +1,119 @@
+#include <gtest/gtest.h>
+#include "genericInputCardReader.h"
+#include "genericInput.h"
+#include <ostream>
+
+const std::string inputPath = "./";
+
+TEST(genericinputcardreadertest, readc0Test)
+{
+    // Given
+    GenericInputCardReader reader(inputPath);
+
+    // Then
+    EXPECT_NEAR(reader.getInput().c_0, 2000.0, 0.0001);
+}
+
+TEST(genericinputcardreadertest, readFreqTest)
+{
+    // Given
+    GenericInputCardReader reader(inputPath);
+    Freq expectedFreq = { 10, 40, 15 };
+    Freq actualFreq = reader.getInput().freq;
+
+    // Then
+    EXPECT_NEAR(actualFreq.min, expectedFreq.min, 0.0001);
+    EXPECT_NEAR(actualFreq.max, expectedFreq.max, 0.0001);
+    EXPECT_NEAR(actualFreq.nTotal, expectedFreq.nTotal, 0.0001);
+}
+
+TEST(genericinputcardreadertest, readReservoirTest)
+{
+    // Given
+    GenericInputCardReader reader(inputPath);
+    std::array<double,2> expectedTopLeft {-300.0, 0.0};
+    std::array<double,2> expectedBottomRight {300.0, 300.0};
+
+    std::array<double,2> actualTopLeft = reader.getInput().reservoirTopLeftCornerInM;
+    std::array<double,2> actualBottomRight = reader.getInput().reservoirBottomRightCornerInM;
+
+
+    // Then
+    EXPECT_NEAR(expectedTopLeft[0], actualTopLeft[0], 0.0001);
+    EXPECT_NEAR(expectedTopLeft[1], actualTopLeft[1], 0.0001);
+
+    EXPECT_NEAR(expectedBottomRight[0], actualBottomRight[0], 0.0001);
+    EXPECT_NEAR(expectedBottomRight[1], actualBottomRight[1], 0.0001);
+}
+
+TEST(genericinputcardreadertest, readSources)
+{
+    // Given
+    GenericInputCardReader reader(inputPath);
+
+    std::array<double,2> expectedTopLeft {-480.0, -5.0};
+    std::array<double,2> expectedBottomRight {480.0, -5.0};
+
+    std::array<double,2> actualTopLeft = reader.getInput().sourcesTopLeftCornerInM;
+    std::array<double,2> actualBottomRight = reader.getInput().sourcesBottomRightCornerInM;
+
+    // Then
+    EXPECT_NEAR(expectedTopLeft[0], actualTopLeft[0], 0.0001);
+    EXPECT_NEAR(expectedTopLeft[1], actualTopLeft[1], 0.0001);
+
+    EXPECT_NEAR(expectedBottomRight[0], actualBottomRight[0], 0.0001);
+    EXPECT_NEAR(expectedBottomRight[1], actualBottomRight[1], 0.0001);
+}
+
+
+TEST(genericinputcardreadertest, readReceivers)
+{
+    // Given
+    GenericInputCardReader reader(inputPath);
+
+    std::array<double,2> expectedTopLeft {-480.0, -5.0};
+    std::array<double,2> expectedBottomRight {480.0, -5.0};
+
+    std::array<double,2> actualTopLeft = reader.getInput().receiversTopLeftCornerInM;
+    std::array<double,2> actualBottomRight = reader.getInput().receiversBottomRightCornerInM;
+
+    // Then
+    EXPECT_NEAR(expectedTopLeft[0], actualTopLeft[0], 0.0001);
+    EXPECT_NEAR(expectedTopLeft[1], actualTopLeft[1], 0.0001);
+
+    EXPECT_NEAR(expectedBottomRight[0], actualBottomRight[0], 0.0001);
+    EXPECT_NEAR(expectedBottomRight[1], actualBottomRight[1], 0.0001);
+}
+
+TEST(genericinputcardreadertest, readNSourcesAndReceiversTest)
+{
+    // Given
+    GenericInputCardReader reader(inputPath);
+    int expectednsources = 17;
+    int expectednreceivers = 17;
+
+    EXPECT_NEAR(reader.getInput().nSourcesReceivers.nsources, expectednsources, 0.0001);
+    EXPECT_NEAR(reader.getInput().nSourcesReceivers.nreceivers, expectednreceivers, 0.0001);
+}
+
+TEST(genericinputcardreadertest, readngridTest)
+{
+    // Given
+    GenericInputCardReader reader(inputPath);
+    std::array<int,2> expectedngrid {64, 32};
+
+    EXPECT_NEAR(reader.getInput().ngrid[0], expectedngrid[0], 0.0001);
+    EXPECT_NEAR(reader.getInput().ngrid[1], expectedngrid[1], 0.0001);
+}
+TEST(genericinputcardreadertest, readngridoriginalTest)
+{
+    // Given
+    GenericInputCardReader reader(inputPath);
+    std::array<int,2> expectedngrid {64, 32};
+
+    EXPECT_NEAR(reader.getInput().ngrid_original[0], expectedngrid[0], 0.0001);
+    EXPECT_NEAR(reader.getInput().ngrid_original[1], expectedngrid[1], 0.0001);
+}
+
+
+
