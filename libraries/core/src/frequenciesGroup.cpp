ff26c32888f73d579f6bb202308e49f845def7bf
valesca Peereboom <valesca.peereboom@alten.nl>
2020-03-23 14:56:58 +0100

    Make a frequency group of one frequency possible

--- libraries/core/src/frequenciesGroup.cpp
+++ libraries/core/src/frequenciesGroup.cpp
@@ -1,14 +1,14 @@
 #include "frequenciesGroup.h"
 #include "log.h"
 
-namespace fwi {
-	static const double pi = std::atan(1.0) * 4.0;
-}
-FrequenciesGroup::FrequenciesGroup(Freq freqStruct, double c_0_)
-    : nFreq(freqStruct.nTotal), c_0(c_0_), d_freq((freqStruct.max-freqStruct.min) / (freqStruct.nTotal - 1)), freq(), k()
+namespace fwi
 {
-    assert(nFreq > 1);
-    for (int i = 0; i < freqStruct.nTotal; i++)
+    static const double pi = std::atan(1.0) * 4.0;
+}
+FrequenciesGroup::FrequenciesGroup(Freq freqStruct, double c_0_) :
+    nFreq(freqStruct.nTotal), c_0(c_0_), d_freq((freqStruct.max - freqStruct.min) / (std::max(freqStruct.nTotal, 2) - 1)), freq(), k()
+{
+    for(int i = 0; i < freqStruct.nTotal; i++)
     {
         freq.push_back(freqStruct.min + i * d_freq);
         double omega = 2.0 * fwi::pi * freq[i];
@@ -18,9 +18,9 @@ FrequenciesGroup::FrequenciesGroup(Freq freqStruct, double c_0_)
 
 void FrequenciesGroup::Print(int nFreqTotal)
 {
-    L_(linfo) << "Total number of frequencies is " << nFreqTotal << ". Values:" ;
-    for (int i = 0; i < nFreqTotal; i++)
+    L_(linfo) << "Total number of frequencies is " << nFreqTotal << ". Values:";
+    for(int i = 0; i < nFreqTotal; i++)
     {
-        L_(linfo) << "f = " << freq[i] << ", k = " << k[i] ;
+        L_(linfo) << "f = " << freq[i] << ", k = " << k[i];
     }
 }
