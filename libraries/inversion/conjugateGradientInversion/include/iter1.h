6438729695563f6c3f0696a5d800921e09b16e28
valesca Peereboom <valesca.peereboom@alten.nl>
2020-04-29 16:33:23 +0200

    Solve small warnings in structs of deltaAmplification and iter1

--- libraries/inversion/conjugateGradientInversion/include/iter1.h
+++ libraries/inversion/conjugateGradientInversion/include/iter1.h
@@ -5,6 +5,6 @@ struct iter1
     int n;
     double tolerance;
 
-    iter1() : n(0), tolerance(0.0){};
-    iter1(int nrOfIterations, double tolerance_) : n(nrOfIterations), tolerance(tolerance_){};
+    iter1() : n(0), tolerance(0.0) {}
+    iter1(int nrOfIterations, double tolerance_) : n(nrOfIterations), tolerance(tolerance_) {}
 };
