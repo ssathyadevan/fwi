212ec727a4ca4d516db34bede587f4dba73a6789
Leonardo Morelli <leonardo.morelli@alten.nl>
2020-07-01 13:30:54 +0200

    augmented integralForwardModelInputCardReaderTest, created folder with invalid inputs

--- libraries/forwardModel/integral/test/integralForwardModelInputCardReaderTest.cpp
+++ libraries/forwardModel/integral/test/integralForwardModelInputCardReaderTest.cpp
@@ -6,17 +6,22 @@ namespace fwi
 {
     namespace forwardModels
     {
-        const std::string inputPath = std::string(FWI_PROJECT_DIR) + "/tests";
-
-        TEST(integralForwardModelInputCardReaderTest, ValidInputTest)
+        const std::string validInputPath = std::string(FWI_PROJECT_DIR) + "/tests";
+        TEST(integralForwardModelInputCardReaderTest, getInput_ValidInput)
         {
-            integralForwardModelInputCardReader integralForwardModelReader(inputPath);
+            integralForwardModelInputCardReader integralForwardModelReader(validInputPath);
             integralForwardModelInput input = integralForwardModelReader.getInput();
 
             EXPECT_EQ(15, input.nrOfIterations);
             EXPECT_EQ(5.0e-5, input.tolerance);
             ASSERT_FALSE(input.calcAlpha);
             EXPECT_EQ(leastSquares, input.costFunction);
+        }
+
+        const std::string invalidInputPath = std::string(FWI_PROJECT_DIR) + "/tests/invalidInput";
+        TEST(integralForwardModelInputCardReaderTest, getInput_InvalidInput_ExceptionsThrown)
+        {   // Only the first invalid input is checked out of two
+            EXPECT_THROW(integralForwardModelInputCardReader integralForwardModelReader(invalidInputPath), std::invalid_argument);
         }
     }   // namespace forwardModels
 }   // namespace fwi
---
+++ tests/invalidInput/README.txt
@@ -0,0 +1,1 @@
+This folder has been created to unit test invalid inputs fed to the card readers without modifying the paths in the CardReader themselves.
