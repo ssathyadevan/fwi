2d70878aef5730324e486be244371ef0b9c744c1
Diego Serrapio <diego.serrapio.lascano@alten.nl>
2020-07-16 12:28:22 +0200

    Review comments

--- libraries/stepAndDirectionMethod/implementation/calculateDirection/include/DummyDirectionCalculator.h
+++ libraries/stepAndDirectionMethod/implementation/calculateDirection/include/DummyDirectionCalculator.h
@@ -6,12 +6,17 @@ namespace fwi
 {
     namespace inversionMethods
     {
+        // Review: Since we are adding a class it would be nice to document its purpose in a general
+        // way like "DummyDirectionCalculator is an inversion method" for doxygen
         class DummyDirectionCalculator : public DirectionCalculator
         {
         public:
-            DummyDirectionCalculator(double errorFunctionalScalingFactor, forwardModels::forwardModelInterface *forwardModel);
+		    // Review: errorFunctionalScalingFactor should be const  and forwardModel should be a const ptr so  forwardModels::forwardModelInterface * const
+            DummyDirectionCalculator( double errorFunctionalScalingFactor, forwardModels::forwardModelInterface *forwardModel);
             virtual ~DummyDirectionCalculator();
 
+            // Review: I think that the documentation is not correct in this case since
+            // calculateDirection always returns zero for this class
             /**
              * @brief calculateDirection computes the direction towards which to move
              * @param chi is a core::dataGrid2D
--- libraries/stepAndDirectionMethod/implementation/calculateDirection/test/DummyDirectionCalculatorTest.cpp
+++ libraries/stepAndDirectionMethod/implementation/calculateDirection/test/DummyDirectionCalculatorTest.cpp
@@ -6,6 +6,9 @@ namespace fwi
 {
     namespace inversionMethods
     {
+        // Review: If getGrid() is meant to be used only in this, file please
+        // put the definition inside a nameless namespace so that it doesn't
+        // pollute the namespace
         core::grid2D getGrid()
         {
             std::array<double, 2> xMin = {0.0, 0.0};
@@ -16,10 +19,21 @@ namespace fwi
             return grid;
         }
 
+        // Review: the test itself looks huge if we consider that it tests that
+        // the class is doing nothing (this is related to how forwardModel and Direction Calculator classes are entangled
+        //  I know it is out of scope but it is something we should think about)
+
+        // Review: I cant really say if this test is fulfilling its purpose because
+        // I am not sure about the purpose of DummyDirectionCalculator
         TEST(DummyDirectionCalculatorTest, calculateDirectionTest)
         {
             // Arrange
             core::grid2D grid = getGrid();
+            // Review: if you need xMin and xMax here again it means that
+            // by also defining nX you could do grid(xMin, xMax, nX) here instead
+            // of using a separate function that only calls core::grid2D constructor
+			
+			// Review: if these are all initialization parameters please make const
             std::array<double, 2> xMin = {0.0, 0.0};
             std::array<double, 2> xMax = {2.0, 2.0};
             core::freqInfo freq(0.0, 10.0, 5);
@@ -27,12 +41,16 @@ namespace fwi
             core::Receivers receivers(xMin, xMax, 2);
             core::FrequenciesGroup frequencies(freq, 2000.0);
 
+			// Review: forwardModel could also be a const ptr 
             forwardModels::forwardModelInterface *forwardModel;
             forwardModel = new forwardModels::ForwardModelInterfaceMock(grid, sources, receivers, frequencies);
 
             double errorFunctionalScalingFactor = 1.0;
+			// Review: directionCalculator could also be a const ptr 
             DirectionCalculator *directionCalculator = new DummyDirectionCalculator(errorFunctionalScalingFactor, forwardModel);
+			// Review: chiEstimate should be const
             core::dataGrid2D chiEstimate(grid);
+			// Review: residuals should be const
             std::vector<std::complex<double>> residuals(grid.getNumberOfGridPoints(), 0.0);
 
             // Act
