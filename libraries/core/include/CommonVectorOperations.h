3c77fd23edf6f475c5de0a6c2f2736276b05a707
Diego Serrapio <diego.serrapio.lascano@alten.nl>
2020-07-30 15:07:30 +0200

    Adds documentation

--- libraries/core/include/CommonVectorOperations.h
+++ libraries/core/include/CommonVectorOperations.h
@@ -10,6 +10,11 @@ namespace fwi
 {
     namespace core
     {
+        /**
+         * @brief Computes the l2 norm squared of a std::vector<std::complex<double>>
+         * @param data is a reference to the vector to use to calculate its l2 norm squared
+         * @return calculated l2 norm squared
+         */
         inline double l2NormSquared(const std::vector<std::complex<double>> &data)
         {
             double initialValue = 0.0;
@@ -17,6 +22,12 @@ namespace fwi
             return std::accumulate(data.begin(), data.end(), initialValue, elementOperation);
         }
 
+        /**
+         * @brief Defines substraction operator between vectors of equal size, it performs element wise substraction
+         * @param operandA constant reference to the first operand
+         * @param operandB constant reference to the second operand
+         * @return a vector where its components are calculated as vector[i] = operandA[i] - operandB[i]
+         */
         template<class type_t> inline std::vector<type_t> operator-(const std::vector<type_t> &operandA, const std::vector<type_t> &operandB)
         {
             assert(operandA.size() == operandB.size() && "Substraction is not defined for vectors of different size");
@@ -25,6 +36,12 @@ namespace fwi
             return result;
         }
 
+        /**
+         * @brief Defines addition operator between vectors of equal size, it performs element wise addition
+         * @param operandA constant reference to the first operand
+         * @param operandB constant reference to the second operand
+         * @return a vector where its components are calculated as vector[i] = operandA[i] + operandB[i]
+         */
         template<class type_t> inline std::vector<type_t> operator+(const std::vector<type_t> &operandA, const std::vector<type_t> &operandB)
         {
             assert(operandA.size() == operandB.size() && "Addition is not defined for vectors of different size");
--- libraries/core/include/CostFunctionCalculator.h
+++ libraries/core/include/CostFunctionCalculator.h
@@ -6,24 +6,44 @@ namespace fwi
 {
     namespace core
     {
+        /**
+         * @brief The CostFunctionCalculator class contains the target function to be optimized
+         */
         class CostFunctionCalculator
         {
         public:
+            /**
+             * @brief The CostFunctionEnum enum Defines the possible cost functions that can be selected
+             */
             enum class CostFunctionEnum
             {
                 leastSquares
             };
-
+            /**
+             * @brief CostFunctionCalculator Class constructor
+             * @param costFunctionSelected sets the function used to calculate the cost, if not defined it will default to CostFunctionEnum::leastSquares
+             */
             explicit CostFunctionCalculator(CostFunctionEnum costFunctionSelected = CostFunctionEnum::leastSquares)
                 : _costFunction(costFunctionSelected)
             {
             }
 
+            /**
+             * @brief calculateCost Calculates cost from current estimation using the defined functions
+             * @param pData reference pressure field data
+             * @param pDataEst current estimation of pressure field
+             * @param eta
+             * @return cost
+             */
             double calculateCost(const std::vector<std::complex<double>> &pData, const std::vector<std::complex<double>> &pDataEst, double eta) const
             {
                 return eta * l2NormSquared(pData - pDataEst);
             }
 
+            /**
+             * @brief getCostFunction Getter function to know the selected cost function
+             * @return selected cost function
+             */
             CostFunctionEnum getCostFunction() const { return _costFunction; }
 
         private:
