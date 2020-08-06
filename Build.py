dee7a152856ddaab30f2cb12bdf7b0f8fe40df01
Saurabh <saurabh.sharma@alten.nl>
2020-08-06 12:25:37 +0200

    updated Build.py to fix gmock issues

--- Build.py
+++ Build.py
@@ -8,12 +8,11 @@ if sys.platform.startswith('linux'):
     current_directory = os.getcwd()
 
     install_gtest()
-
     print('Build project:')
     if not os.path.isdir(current_directory + '/build'):
         os.mkdir('build')
     os.chdir(current_directory + '/build')
-    check = os.system('sudo cmake -DCMAKE_BUILD_TYPE=Release ' +
+    check = os.system('cmake -DCMAKE_BUILD_TYPE=Release ' +
                     '-DCMAKE_INSTALL_PREFIX=../FWIInstall ../')
     checking_for_errors(check, current_directory)
     check = os.system('sudo make install')
