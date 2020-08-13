7763317eeee859132da14a25abf2a3cbfecaf000
Diego Serrapio <diego.serrapio.lascano@alten.nl>
2020-08-13 10:02:07 +0200

    Cleanup jenkinsfiles

--- continuousIntegration/functions.groovy
+++ continuousIntegration/functions.groovy
@@ -15,54 +15,77 @@ def setEnvironment() {
         env.COMITTER_EMAIL = sh(returnStdout: true, script: 'git --no-pager show -s --format=\'%ae\'').trim()
         env.AUTHOR_NAME = sh(returnStdout: true, script: 'git --no-pager show -s --format=\'%an\'').trim()
         env.MYSTAGE_NAME = 'Preparing'
+		
 		// Set build name and description accordingly
         currentBuild.displayName = "FWI | commit ${SHORT_COMMIT_CODE} | ${AUTHOR_NAME}"
         currentBuild.description = "${COMMIT_MESSAGE}"
 }
 
-def buildAll() {
-        echo 'Building..'
+def build(String osName = "undefined") {
+        echo 'Building on' + osName
 		env.MYSTAGE_NAME = 'Build'
-        sh '''
-        mkdir build
-        cd build
-        cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${WORKSPACE}/FWIInstall ..
-        make install
-        '''
+		if (osName == "Windows"){
+			bat(script:'mkdir build \n cd build \n cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../FWIInstall ..\n ninja install')
+		}
+		else{
+		    sh '''
+			mkdir build
+			cd build
+			cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${WORKSPACE}/FWIInstall ..
+			make install
+			'''
+		}
+
         //cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/var/jenkins_home/workspace/FWI/${GIT_BRANCH}/FWIInstall ..
 }
 
-def testAll() {
-	    echo 'testing all'
-    	env.MYSTAGE_NAME = 'Test'
-    	sh '''
-		cd build
-    	make test
-    	ctest -T test --no-compress-output
-    	cp Testing/`head -n 1 Testing/TAG`/Test.xml ./CTestResults.xml
-    	'''
+def unitTest(String osName = "undefined") {
+	    echo 'Running unit tests on' + osName
+    	env.MYSTAGE_NAME = 'Unit Testing'
+		if (osName == "Windows"){
+			bat(script:"cd build \n ctest -T test --no-compress-output")
+			bat(script: 'for /f "delims=" %%i in (\'where /r %WORKSPACE%\\build Test.xml\') do set dirOutput=%%i \n cd build \n echo %dirOutput% \n copy %dirOutput% .\\CTestResults.xml')
+		}
+		else{
+			sh '''
+			cd build
+			make test
+			ctest -T test --no-compress-output
+			cp Testing/`head -n 1 Testing/TAG`/Test.xml ./CTestResults.xml
+			'''
+		}
 }
 
-def regressiontest() {
-        echo 'Running regression tests'
+def regressionTest(String osName = "undefined") {
+        echo 'Running regression tests on' + osName
 		env.MYSTAGE_NAME = 'Regression Testing'
-		sh '''
-		cp tests/testScripts/unified_run_all_regressions_python.py .
-		python3 unified_run_all_regressions_python.py IntegralForwardModel ConjugateGradientInversion
-		'''
+		if (osName == "Windows"){
+			bat(script:'copy %WORKSPACE%\\tests\\testScripts\\unified_run_all_regressions_python.py . \n python3 unified_run_all_regressions_python.py IntegralForwardModel ConjugateGradientInversion')		
+		}
+		else{
+			sh '''
+			cp tests/testScripts/unified_run_all_regressions_python.py .
+			python3 unified_run_all_regressions_python.py IntegralForwardModel ConjugateGradientInversion
+			'''
+		}
 }
 
-def deploy(){
-        echo 'Deploying'
+def deploy(String osName = "undefined"){
+        echo 'Deploying on' + osName
 		env.MYSTAGE_NAME = 'Deploy'
-        sh '''
-        cp -r inputFiles FWIInstall/
-		mkdir FWIInstall/pythonScripts
-        cp -r pythonScripts/* FWIInstall/pythonScripts
-        tar -zcf Ubuntu-FWI-${GIT_BRANCH}-${SHORT_COMMIT_CODE}.tar.gz FWIInstall
-        '''
-        archiveArtifacts artifacts:"Ubuntu-FWI-${GIT_BRANCH}-${SHORT_COMMIT_CODE}.tar.gz"
-
+		if (osName == "Windows"){
+			bat(script:'xcopy inputFiles FWIInstall\\inputFiles /s /i \n xcopy pythonScripts FWIInstall\\pythonScripts /s /i \n 7z a Windows-FWI-%GIT_BRANCH%-%SHORT_COMMIT_CODE%.zip ./FWIInstall/*')
+			archiveArtifacts artifacts:"Windows-FWI-${GIT_BRANCH}-${SHORT_COMMIT_CODE}.zip"	
+		}
+		else{
+			sh '''
+			cp -r inputFiles FWIInstall/
+			mkdir FWIInstall/pythonScripts
+			cp -r pythonScripts/* FWIInstall/pythonScripts
+			tar -zcf Ubuntu-FWI-${GIT_BRANCH}-${SHORT_COMMIT_CODE}.tar.gz FWIInstall
+			'''
+			archiveArtifacts artifacts:"Ubuntu-FWI-${GIT_BRANCH}-${SHORT_COMMIT_CODE}.tar.gz"
+		}
 }
 
 def unitTestSummary() {
