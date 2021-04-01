105df1a5e9b4bb7460b67be76e69f119b698135c
Bas Dirkse <bas.dirkse@alten.nl>
2021-04-01 14:40:11 +0200

    User Story 14643 input arguments improved

---
+++ libraries/io/include/argumentReader.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+namespace fwi
+{
+    namespace io
+    {
+        struct argumentReader {
+            std::string dir;
+            std::string forward;
+            std::string inversion;
+            std::string stepdir;
+            std::string stepsize;
+            bool help;
+            bool version;
+
+            argumentReader() = delete;
+            argumentReader(std::vector<std::string>& arguments);
+        };
+    }
+}
